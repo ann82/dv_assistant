3ecfa9764e50a8be325c2eee3cc1a6178664fe36
Ann Abraham <annabraham82@gmail.com>
2025-06-06 14:37:03 -0700

    fix: Update module resolution and imports

---
+++ relay-server/lib/index.js
@@ -0,0 +1,7 @@
+export * from './intentClassifier.js';
+export * from './config.js';
+export * from './twilio.js';
+export * from './twilioVoice.js';
+export * from './logger.js';
+export * from './apis.js';
+export * from './utils.js'; 
